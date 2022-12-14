d4822c08a35a524d63f3653349bdbaabbd7c9d49
armod <armods@gmail.com>
2022-12-14 11:35:29 +0100

    css modules

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Layout from '../components/Layout'
-
+import { page, text } from '../examples/about.module.css'
 const about = () => {
   return (
     <Layout>
-      <div className='about-heading'>
+      <div className={page}>
         <h1>about page</h1>
+        <h1>hello world</h1>
+
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nisi
+          vitae vel aperiam nihil totam placeat officia at in neque, impedit ea
+          unde harum ullam distinctio libero ratione fugiat pariatur praesentium
+          illo adipisci id, illum maxime atque! Id, necessitatibus quod?
+        </p>
       </div>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,13 @@ export default function Home() {
         <h1>home page</h1>
         <h1>hello world</h1>
         <h1>hello people</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium
+          provident quam, temporibus autem blanditiis ipsam harum ipsum hic!
+          Dolores voluptatum veritatis earum saepe, accusamus reiciendis dolorum
+          illo nemo perspiciatis repellat minima quasi sunt illum hic nostrum
+          quia, numquam dolorem quibusdam!
+        </p>
       </div>
     </Layout>
   )
