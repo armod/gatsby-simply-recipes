e0f4f7a8b739e2b82e9b27db7e9548064fd419f8
armod <armods@gmail.com>
2022-12-14 15:00:15 +0100

    cleaning

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,34 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import { page, text } from '../examples/about.module.css'
-import styled from 'styled-components'
+
 const about = () => {
   return (
     <Layout>
-      <Wrapper>
-        <h1>about page</h1>
-        <h1>hello world</h1>
-        <p className={text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nisi
-          vitae vel aperiam nihil totam placeat officia at in neque, impedit ea
-          unde harum ullam distinctio libero ratione fugiat pariatur praesentium
-          illo adipisci id, illum maxime atque! Id, necessitatibus quod?
-        </p>
-      </Wrapper>
+      <h1>about page</h1>
     </Layout>
   )
 }
-
-const Wrapper = styled.section`
-  color: blue;
-
-  h1 {
-    color: yellowgreen;
-  }
-
-  .text {
-    text-transform: uppercase;
-  }
-`
 
 export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,10 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
-import { page, text } from '../examples/home.module.css'
-import { ExampleButton } from '../examples/button'
 
 export default function Home() {
   return (
     <Layout>
-      <div className={page}>
-        <ExampleButton>click me</ExampleButton>
-        <h1>home page</h1>
-        <h1>hello world</h1>
-        <h1>hello people</h1>
-        <p className={text}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium
-          provident quam, temporibus autem blanditiis ipsam harum ipsum hic!
-          Dolores voluptatum veritatis earum saepe, accusamus reiciendis dolorum
-          illo nemo perspiciatis repellat minima quasi sunt illum hic nostrum
-          quia, numquam dolorem quibusdam!
-        </p>
-      </div>
+      <h1>home page</h1>
     </Layout>
   )
 }
