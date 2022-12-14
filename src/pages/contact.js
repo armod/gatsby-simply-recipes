92a0919fba755caf8ccaf9833bb75b21da4e4ddc
armod <armods@gmail.com>
2022-12-14 15:18:42 +0100

    contact page - szablon

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,42 @@ import Layout from '../components/Layout'
 const Contact = () => {
   return (
     <Layout>
-      <h1>contact page</h1>
+      <main className='page'>
+        <section className='contact-page'>
+          <article className='contact-info'>
+            <h3>Want to get in touch?</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
+              minima animi corrupti architecto, fuga tenetur explicabo adipisci
+              aperiam dolorum at! Cupiditate, accusamus maxime repellendus
+            </p>
+            <p>
+              nesciunt nihil quam consequuntur reiciendis facilis numquam
+              quaerat! Et rem perferendis unde ipsum inventore, repellendus
+              quia.
+            </p>
+          </article>
+          <article>
+            <form className='form contanct-form'>
+              <div className='form-row'>
+                <label htmlFor='name'>your name</label>
+                <input type='text' name='name' id='name' />
+              </div>
+              <div className='form-row'>
+                <label htmlFor='email'>your email</label>
+                <input type='text' name='email' id='email' />
+              </div>
+              <div className='form-row'>
+                <label htmlFor='message'>message</label>
+                <textarea name='message' id='message'></textarea>
+              </div>
+              <button type='submit' className='btn block'>
+                submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   )
 }
