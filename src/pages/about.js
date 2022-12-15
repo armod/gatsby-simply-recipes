872f89df5215da84a7c3e91c82c16c84b3a29afb
armod <armods@gmail.com>
2022-12-15 14:57:38 +0100

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 
-const about = () => {
+const About = () => {
   return (
     <Layout>
-      <h1>about page</h1>
+      <main className='page'>
+        <section className='about-page'>
+          <article>
+            <h2>I'm baby coloring book poke taxidermy</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+              eum quasi corporis similique accusantium, repellendus
+              necessitatibus provident beatae quos doloribus.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              ipsam fugiat perspiciatis ipsa dolore non odio iste nam expedita
+              aliquid.
+            </p>
+            <Link to='/contact' className='btn'>
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src='../assets/images/about.jpeg'
+            alt='Person Pouring Salt in Blow'
+            className='about-img'
+            placeholder='blurred'
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
 
-export default about
+export default About
