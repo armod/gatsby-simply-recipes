6ec957fbd2e03b3e616d972d1bfc511030b621d2
armod <armods@gmail.com>
2022-12-21 11:54:37 +0100

    all recepies

--- src/examples/AllRecepies.js
+++ src/examples/AllRecepies.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import TagsList from './TagsList'
 import ReciepiesList from './ReciepiesList'
+import { graphql, useStaticQuery } from 'gatsby'
+
+const query = graphql`
+  query {
+    allContentfulRecipe(sort: { title: ASC }) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
 
 const AllRecepies = () => {
+  const data = useStaticQuery(query)
+  const recipes = data.allContentfulRecipe.nodes
+  // console.log(data)
   return (
     <div>
       <h4>all recepies</h4>
-      <TagsList />
+      <TagsList recipes={recipes} />
       <ReciepiesList />
     </div>
   )
